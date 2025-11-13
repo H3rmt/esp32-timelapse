84e0926469de0a66e0e31f34ec29b29e170a6d48
Enrico Stemmer <enrico@h3rmt.zip>
2025-11-14 00:30:14 +0100

    feat: add test at startup
    fix: update deps

--- server/src/export.rs
+++ server/src/export.rs
@@ -3,7 +3,7 @@ use tokio::process::Command;
 
 pub async fn export(path: String, fps: String) -> Result<String, Error> {
     let mut cmd = Command::new("ffmpeg");
-    cmd.arg("-framerate").arg(fps).arg("-i").arg(format!("{path}/%d.jpg")).arg("-c:v").arg("libx264").arg("-preset").arg("slow").arg("-crf").arg("18").arg(format!("{path}/out.mp4"));
+    cmd.arg("-pattern_type").arg("glob").arg("-framerate").arg(fps).arg("-i").arg(format!("{path}/*.jpg")).arg("-c:v").arg("libx264").arg("-preset").arg("slow").arg("-crf").arg("18").arg(format!("{path}/out.mp4"));
     println!("Running ffmpeg...{cmd:?}");
     cmd.output().await.map_err(|e| {
         eprintln!("Error: Could not run ffmpeg ({e})");
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,6 @@
 mod export;
 
-use std::env;
-use std::net::SocketAddr;
+use std::{env, fs};
 use axum::{routing::get, routing::post, Router, debug_handler};
 use axum::body::Bytes;
 use axum::extract::{Query};
@@ -12,10 +11,13 @@ use rand::Rng;
 use serde::Deserialize;
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
+use tokio::process::Command;
+
+const FOLDER_NAME: &str = "images";
 
 #[tokio::main]
 async fn main() {
-    env::var("FPS").expect("Error: FPS not found");
+    test().await;
 
     let app = Router::new()
         .route("/upload", post(upload))
@@ -25,6 +27,18 @@ async fn main() {
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.expect("server failed to start");
     println!("listening on {:?}", listener);
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn test() {
+    env::var("FPS").expect("Error: FPS not found");
+
+    let mut file = File::create(format!("{FOLDER_NAME}/test")).await.expect("Cant create test file");
+    file.write_all(b"test").await.expect("Cant write to test file");
+    fs::remove_file(format!("{FOLDER_NAME}/test")).expect("Cant remove test file");
+
+    let mut cmd = Command::new("ffmpeg");
+    cmd.arg("-h");
+    cmd.output().await.expect("Cant run ffmpeg");
 }
 
 #[derive(Debug, Deserialize)]
@@ -45,7 +59,7 @@ async fn finish(Query(params): Query<Params>) -> impl IntoResponse {
         StatusCode::BAD_REQUEST
     })?;
 
-    std::fs::rename(format!("imgs/{identifier}"), format!("imgs/{identifier}-finished-{count}")).map_err(|e| {
+    fs::rename(format!("{FOLDER_NAME}/{identifier}"), format!("{FOLDER_NAME}/{identifier}-finished-{count}")).map_err(|e| {
         eprintln!("Error: Could not rename folder ({e})");
         StatusCode::INTERNAL_SERVER_ERROR
     })?;
@@ -58,7 +72,7 @@ async fn finish(Query(params): Query<Params>) -> impl IntoResponse {
     })?;
 
     println!("Exporting video...");
-    let path = export::export(format!("imgs/{identifier}-finished-{count}"), fps).await.map_err(|e| {
+    let path = export::export(format!("{FOLDER_NAME}/{identifier}-finished-{count}"), fps).await.map_err(|e| {
         eprintln!("Error: Could not export video ({e})");
         StatusCode::INTERNAL_SERVER_ERROR
     })?;
@@ -69,10 +83,10 @@ async fn finish(Query(params): Query<Params>) -> impl IntoResponse {
 
 async fn start() -> Result<String, StatusCode> {
     let time_str = Local::now().format("%y-%m-%d");
-    let mut rng = rand::thread_rng();
-    let identifier = format!("{time_str}_{}", rng.gen_range(100..999));
+    let mut rng = rand::rng();
+    let identifier = format!("{time_str}_{}", rng.random_range(100..999));
 
-    std::fs::create_dir(format!("imgs/{identifier}")).map_err(|e| {
+    fs::create_dir_all(format!("{FOLDER_NAME}/{identifier}")).map_err(|e| {
         eprintln!("Error: Could not create folder ({e})");
         StatusCode::INTERNAL_SERVER_ERROR
     })?;
@@ -84,19 +98,22 @@ async fn start() -> Result<String, StatusCode> {
 #[debug_handler]
 async fn upload(Query(params): Query<Params>, body: Bytes) -> impl IntoResponse {
     let time_str = Local::now().format("%y-%m-%d_%H:%M:%S");
+    println!("Received image: ({time_str})");
 
     let count = params.count.ok_or_else(|| {
         eprintln!("Error: Count param not found");
         StatusCode::BAD_REQUEST
     })?;
+    // pad count to 4 digits
+    let count = format!("{:04}", count);
 
     let identifier = params.identifier.ok_or_else(|| {
         eprintln!("Error: Identifier param not found");
         StatusCode::BAD_REQUEST
     })?;
 
-    let mut file = File::create(format!("imgs/{identifier}/{count}.jpg")).await.map_err(|e| {
-        eprintln!("Error: Could not create file({}) ({e})", format!("imgs/{identifier}/{count}.jpg"));
+    let mut file = File::create(format!("{FOLDER_NAME}/{identifier}/{count}.jpg")).await.map_err(|e| {
+        eprintln!("Error: Could not create file({}) ({e})", format!("{FOLDER_NAME}/{identifier}/{count}.jpg"));
         StatusCode::INTERNAL_SERVER_ERROR
     })?;
 
