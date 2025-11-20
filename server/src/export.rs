ebd97ae824568b3c0961a24650d50acaf790f418
Enrico Stemmer <enrico@h3rmt.zip>
2025-11-20 21:36:19 +0100

    feat: fetter logging

--- server/src/export.rs
+++ server/src/export.rs
@@ -4,12 +4,12 @@ use tokio::process::Command;
 pub async fn export(path: String, fps: String) -> Result<String, Error> {
     let mut cmd = Command::new("ffmpeg");
     cmd.arg("-pattern_type").arg("glob").arg("-framerate").arg(fps).arg("-i").arg(format!("{path}/*.jpg")).arg("-c:v").arg("libx264").arg("-preset").arg("slow").arg("-crf").arg("18").arg(format!("{path}/out.mp4"));
-    println!("Running ffmpeg...{cmd:?}");
+    log::debug!("Running ffmpeg...{cmd:?}");
     cmd.output().await.map_err(|e| {
-        eprintln!("Error: Could not run ffmpeg ({e})");
+        log::warn!("Error: Could not run ffmpeg ({e})");
         e
     }).map(|d| {
-        println!("Data: {d:?}");
+        log::debug!("Data: {d:?}");
     })?;
 
     Ok(format!("{path}/out.mp4"))
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,14 @@
 mod export;
 
-use std::{env, fs};
-use axum::{routing::get, routing::post, Router, debug_handler};
 use axum::body::Bytes;
-use axum::extract::{Query};
-use chrono::offset::Local;
-use axum::http::{StatusCode};
+use axum::extract::Query;
+use axum::http::StatusCode;
 use axum::response::IntoResponse;
+use axum::{debug_handler, routing::get, routing::post, Router};
+use chrono::offset::Local;
 use rand::Rng;
 use serde::Deserialize;
+use std::{env, fs};
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
 use tokio::process::Command;
@@ -17,6 +17,8 @@ const FOLDER_NAME: &str = "images";
 
 #[tokio::main]
 async fn main() {
+    env_logger::init();
+
     test().await;
 
     let app = Router::new()
@@ -24,21 +26,34 @@ async fn main() {
         .route("/finish", get(finish))
         .route("/start", get(start));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.expect("server failed to start");
-    println!("listening on {:?}", listener);
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080")
+        .await
+        .expect("server failed to start");
+    log::info!("listening on {:?}", listener);
     axum::serve(listener, app).await.unwrap();
 }
 
 async fn test() {
     env::var("FPS").expect("Error: FPS not found");
+    log::debug!("FPS env found");
 
-    let mut file = File::create(format!("{FOLDER_NAME}/test")).await.expect("Cant create test file");
-    file.write_all(b"test").await.expect("Cant write to test file");
+    fs::create_dir_all(FOLDER_NAME).expect("Cant create folder");
+    let mut file = File::create(format!("{FOLDER_NAME}/test"))
+        .await
+        .expect("Cant create test file");
+    file.write_all(b"test")
+        .await
+        .expect("Cant write to test file");
     fs::remove_file(format!("{FOLDER_NAME}/test")).expect("Cant remove test file");
+    log::debug!("test file created");
+
+    fs::create_dir_all(format!("{FOLDER_NAME}/00000000000000"))
+        .expect("Cant create fallback folder");
 
     let mut cmd = Command::new("ffmpeg");
     cmd.arg("-h");
     cmd.output().await.expect("Cant run ffmpeg");
+    log::debug!("ffmpeg found");
 }
 
 #[derive(Debug, Deserialize)]
@@ -50,34 +65,40 @@ struct Params {
 
 async fn finish(Query(params): Query<Params>) -> impl IntoResponse {
     let count = params.count.ok_or_else(|| {
-        eprintln!("Error: Count param not found");
+        log::warn!("Error: Count param not found");
         StatusCode::BAD_REQUEST
     })?;
 
     let identifier = params.identifier.ok_or_else(|| {
-        eprintln!("Error: Identifier param not found");
+        log::warn!("Error: Identifier param not found");
         StatusCode::BAD_REQUEST
     })?;
 
-    fs::rename(format!("{FOLDER_NAME}/{identifier}"), format!("{FOLDER_NAME}/{identifier}-finished-{count}")).map_err(|e| {
-        eprintln!("Error: Could not rename folder ({e})");
+    fs::rename(
+        format!("{FOLDER_NAME}/{identifier}"),
+        format!("{FOLDER_NAME}/{identifier}-finished-{count}"),
+    )
+    .map_err(|e| {
+        log::warn!("Error: Could not rename folder ({e})");
         StatusCode::INTERNAL_SERVER_ERROR
     })?;
 
-    println!("Finished Folder {identifier}-{count}");
+    log::info!("Finished Folder {identifier}-{count}");
 
     let fps = env::var("FPS").map_err(|e| {
-        eprintln!("Error: FPS not found ({e})");
+        log::warn!("Error: FPS not found ({e})");
         StatusCode::INTERNAL_SERVER_ERROR
     })?;
 
-    println!("Exporting video...");
-    let path = export::export(format!("{FOLDER_NAME}/{identifier}-finished-{count}"), fps).await.map_err(|e| {
-        eprintln!("Error: Could not export video ({e})");
-        StatusCode::INTERNAL_SERVER_ERROR
-    })?;
+    log::info!("Exporting video...");
+    let path = export::export(format!("{FOLDER_NAME}/{identifier}-finished-{count}"), fps)
+        .await
+        .map_err(|e| {
+            log::warn!("Error: Could not export video ({e})");
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?;
 
-    println!("Finished exporting video {path}");
+    log::info!("Finished exporting video {path}");
     Ok::<(), axum::response::ErrorResponse>(())
 }
 
@@ -87,41 +108,46 @@ async fn start() -> Result<String, StatusCode> {
     let identifier = format!("{time_str}_{}", rng.random_range(100..999));
 
     fs::create_dir_all(format!("{FOLDER_NAME}/{identifier}")).map_err(|e| {
-        eprintln!("Error: Could not create folder ({e})");
+        log::warn!("Error: Could not create folder ({e})");
         StatusCode::INTERNAL_SERVER_ERROR
     })?;
 
-    println!("Created new folder: {identifier}");
+    log::info!("Created new folder: {identifier}");
     Ok(identifier)
 }
 
 #[debug_handler]
 async fn upload(Query(params): Query<Params>, body: Bytes) -> impl IntoResponse {
     let time_str = Local::now().format("%y-%m-%d_%H:%M:%S");
-    println!("Received image: ({time_str})");
+    log::info!("Received image: ({time_str})");
 
     let count = params.count.ok_or_else(|| {
-        eprintln!("Error: Count param not found");
+        log::warn!("Error: Count param not found");
         StatusCode::BAD_REQUEST
     })?;
     // pad count to 4 digits
     let count = format!("{:04}", count);
 
     let identifier = params.identifier.ok_or_else(|| {
-        eprintln!("Error: Identifier param not found");
+        log::warn!("Error: Identifier param not found");
         StatusCode::BAD_REQUEST
     })?;
 
-    let mut file = File::create(format!("{FOLDER_NAME}/{identifier}/{count}.jpg")).await.map_err(|e| {
-        eprintln!("Error: Could not create file({}) ({e})", format!("{FOLDER_NAME}/{identifier}/{count}.jpg"));
-        StatusCode::INTERNAL_SERVER_ERROR
-    })?;
+    let mut file = File::create(format!("{FOLDER_NAME}/{identifier}/{count}.jpg"))
+        .await
+        .map_err(|e| {
+            log::warn!(
+                "Error: Could not create file({}) ({e})",
+                format!("{FOLDER_NAME}/{identifier}/{count}.jpg")
+            );
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?;
 
     file.write_all(&body).await.map_err(|e| {
-        eprintln!("Error: Could not write to file({:?}) ({e})", file);
+        log::warn!("Error: Could not write to file({:?}) ({e})", file);
         StatusCode::INTERNAL_SERVER_ERROR
     })?;
 
-    println!("Saved image: ({time_str}): {count}.jpg");
+    log::info!("Saved image: ({time_str}): {count}.jpg");
     Ok::<(), axum::response::ErrorResponse>(())
 }
