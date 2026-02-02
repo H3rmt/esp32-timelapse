e1137e31a7ec966a6696b4433d2a78bdeef32b29
Enrico Stemmer <enrico@h3rmt.dev>
2026-02-02 14:33:55 +0100

    feat: update ffmpeg command

--- server/src/export.rs
+++ server/src/export.rs
@@ -5,14 +5,37 @@ use tracing::log::warn;
 
 pub async fn export(path: &str, fps: &str, name_reg: &str, out: &str) -> Result<String, Error> {
     let mut cmd = Command::new("ffmpeg");
-    cmd.arg("-pattern_type").arg("glob").arg("-framerate").arg(fps).arg("-i").arg(format!("{path}/{name_reg}")).arg("-c:v").arg("libx264").arg("-preset").arg("slow").arg("-crf").arg("18").arg(format!("{path}/{out}.mp4"));
+    cmd.arg("-pattern_type")
+        .arg("glob")
+        .arg("-framerate")
+        .arg(fps)
+        .arg("-i")
+        .arg(format!("{path}/{name_reg}"))
+        .arg("-c:v")
+        .arg("libx264")
+        .arg("-crf")
+        .arg("18")
+        .arg("-preset")
+        .arg("slow")
+        .arg("-pix_fmt")
+        .arg("yuv420p")
+        .arg("-profile:v")
+        .arg("high")
+        .arg("-level")
+        .arg("4.0")
+        .arg("-movflags")
+        .arg("+faststart")
+        .arg(format!("{path}/{out}.mp4"));
     debug!("Running ffmpeg...{cmd:?}");
-    cmd.output().await.map_err(|e| {
-        warn!("Error: Could not run ffmpeg ({e})");
-        e
-    }).map(|d| {
-        debug!("Data: {d:?}");
-    })?;
+    cmd.output()
+        .await
+        .map_err(|e| {
+            warn!("Error: Could not run ffmpeg ({e})");
+            e
+        })
+        .map(|d| {
+            debug!("Data: {d:?}");
+        })?;
 
     Ok(format!("{path}/out.mp4"))
 }
